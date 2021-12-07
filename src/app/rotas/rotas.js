aae2ba2a9e38f2c955e42e959baec57710942948
ayrtonmoises <ayrton.sousa@gmail.com>
2021-12-07 15:14:07 -0300

    Aula 3

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,35 +1,37 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
 module.exports = (app) => {
 
     app.get('/', function (req, resp) {
         resp.send(
             `
             <html>
-            <head>
-            </head>
-            <body>
-            <h1>Casa do codigo</h1>
-            </body>
+                <head>
+                </head>
+                <body>
+                    <h1>Casa do codigo</h1>
+                </body>
             </html>
             `
         );
     });
 
     app.get('/livros', function (req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node avançado'
-                    }
-                ]
-            }
-        );
+
+        const livroDao = new LivroDao(db);
+
+        livroDao.lista()
+            .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros
+                }
+            ))
+            .catch(erro => console.log(erro));
+
     });
+
+
 };
 
